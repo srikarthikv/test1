036aa98bf1756f00b4993ce3bc26d103cfa664bf
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-05-31 13:09:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,43 +8,13 @@
     <p>This is a PHP web app that sends a GET request.</p>
 
     <?php
+    // Send GET request
+    $url = 'http://10.1.0.4:8000';
+    $response = file_get_contents($url);
 
- 
-
-function curlTwo() {
-  $absPath = readline("Please enter the path: "); // Prompt user for input
-
- 
-
-  $url = 'http://10.1.0.4:8000/embedding'; // Enter your IP address/host here
-  $options = array(
-    'http' => array(
-      'method' => 'POST',
-      'header' => 'Content-type: application/json; charset=UTF-8',
-      'content' => json_encode(array(
-        'document' => $absPath
-      ))
-    )
-  );
-
- 
-
-  $context = stream_context_create($options);
-  $response = file_get_contents($url, false, $context);
-
- 
-
-  if ($response === false) {
-    echo "Error fetching data.";
-  } else {
-    $data = json_decode($response);
-    echo json_encode($data);
-  }
-}
-
- 
-
-curlTwo();
-?>
+    // Display response
+    echo '<p>Response:</p>';
+    echo '<pre>' . $response . '</pre>';
+    ?>
 </body>
 </html>
