cd7964f97ad3617331403d118e7de10be955518e
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-05-31 23:56:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -53,8 +53,8 @@
         <label for="question">Question:</label>
         <input type="text" name="question" id="question" required><br>
 
-        <label for="document">Document Path:</label>
-        <input type="text" name="document" id="document" required><br>
+        //<label for="document">Document Path:</label>
+        //<input type="text" name="document" id="document" required><br>
 
         <button type="submit">Submit</button>
     </form>
