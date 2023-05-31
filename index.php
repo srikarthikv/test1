5dea4412e548fd23c9ccd02649499238ec22b007
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-05-31 18:56:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,21 @@
     <p>This is a PHP web app that sends a GET request.</p>
 
     <?php
-    // Send GET request
-    $url = 'http://10.1.0.4:8000';
-    $response = file_get_contents($url);
 
-    // Display response
-    echo '<p>Response:</p>';
-    echo '<pre>' . $response . '</pre>';
+    // Prompt for question and document path
+    $question = readline("Enter the question: ");
+    $document = readline("Enter the document path: ");
+
+    // Set the command to be executed
+    $command = 'curl -X GET "http://10.1.0.4:8000/qanda?question=' . urlencode($question) . '&document=' . urlencode($document) . '"';
+
+    // Execute the command and capture the output
+    $output = exec($command);
+
+    // Output the response
+    echo $output;
+
     ?>
+
 </body>
 </html>
