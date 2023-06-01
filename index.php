744d2878044f70f9b6151cab3f62cfb690ba83b6
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-06-01 15:24:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1>Hello, PHP!</h1>
-    <p>This is a PHP web app that sends a GET request.</p>
+    <p>This is a PHP web app that sends a POST request.</p>
 
     <?php
 
@@ -13,16 +13,48 @@
     $question = readline("Enter the question: ");
     $document = readline("Enter the document path: ");
 
-    // Set the command to be executed
-    $command = 'curl -X GET "http://10.1.0.4:8000/qanda?question=' . urlencode($question) . '&document=' . urlencode($document) . '"';
+    // Set the request URL
+    $url = 'http://10.1.0.4:8000/qanda';
 
-    // Execute the command and capture the output
-    $output = exec($command);
+    // Set the request data
+    $data = array(
+        'question' => $question,
+        'document' => $document
+    );
 
-    // Output the response
-    echo $output;
+    // Initialize cURL session
+    $ch = curl_init();
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+    // Execute the request
+    $response = curl_exec($ch);
+
+    // Check for errors
+    if (curl_errno($ch)) {
+        echo 'Error: ' . curl_error($ch);
+    } else {
+        // Output the response
+        echo $response;
+    }
+
+    // Close cURL session
+    curl_close($ch);
 
     ?>
+
+    <form action="qanda" method="POST">
+        <label for="question">Question:</label>
+        <input type="text" name="question" id="question" required><br>
+
+        <label for="document">Document Path:</label>
+        <input type="text" name="document" id="document" required><br>
+
+        <button type="submit">Submit</button>
+    </form>
 
 </body>
 </html>
