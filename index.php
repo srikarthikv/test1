14aaaf6b9308263d18b8e09538a860f992d94f2f
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-06-01 15:51:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,45 +8,45 @@
     <p>This is a PHP web app that sends a POST request.</p>
 
     <?php
-    // Prompt for question
-    $question = readline("Enter the question: ");
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Retrieve the question from the form
+        $question = $_POST['question'];
 
-    // Set the request URL
-    $url = 'http://10.1.0.4:8000/qanda';
+        // Set the request URL
+        $url = 'http://10.1.0.4:8000/qanda';
 
-    // Set the request data
-   // $data =        'question' => $question,
-   
+        // Set the request data
+        $data = array(
+            'question' => $question,
+        );
 
-    // Initialize cURL session
-    $ch = curl_init();
+        // Initialize cURL session
+        $ch = curl_init();
 
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($question));
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 
-    // Execute the request
-    $response = curl_exec($ch);
+        // Execute the request
+        $response = curl_exec($ch);
 
-    // Check for errors
-    if (curl_errno($ch)) {
-        echo 'Error: ' . curl_error($ch);
-    } else {
-        // Output the response
-        echo $response;
+        // Check for errors
+        if (curl_errno($ch)) {
+            echo 'Error: ' . curl_error($ch);
+        } else {
+            // Output the response
+            echo $response;
+        }
+
+        // Close cURL session
+        curl_close($ch);
     }
-
-    // Close cURL session
-    curl_close($ch);
     ?>
 
-    <form action="qanda" method="POST">
+    <form action="" method="POST">
         <label for="question">Question:</label>
         <input type="text" name="question" id="question" required><br>
-
-        <label for="document">Document Filename:</label>
-        <input type="text" name="document" id="document" required><br>
 
         <button type="submit">Submit</button>
     </form>
