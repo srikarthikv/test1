2f9bce4b1e5c0be7b0deb925fc8777552e122ad4
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-06-02 11:49:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,51 +4,44 @@
     <title>PHP Web App</title>
 </head>
 <body>
-    <h1>Hello, PHP!</h1>
-    <p>This is a PHP web app that sends a POST request.</p>
+    <h1>Hello, ChstGPT Demo!</h1>
+    <p>This is a PHP web app that sends a GET/POST request to the Azure ChatGPT Engine.</p>
 
     <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Retrieve the question from the form
-        $question = $_POST['question'];
 
-        // Set the request URL
-        $url = 'http://10.1.0.4:8000/qanda';
-
-        // Set the request data
-        $data = array(
-            'question' => $question,
-        );
-
-        // Initialize cURL session
-        $ch = curl_init();
-
-        // Set cURL options
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-
-        // Execute the request
-        $response = curl_exec($ch);
-
-        // Check for errors
-        if (curl_errno($ch)) {
-            echo 'Error: ' . curl_error($ch);
-        } else {
-            // Output the response
-            echo $response;
-        }
-
-        // Close cURL session
-        curl_close($ch);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $data = json_decode(file_get_contents('php://input'), true);
+      print_r($data);
+      // Find out the request
+      $request = $_SERVER['REQUEST_URI'];
+      // Processing part of the Embedding
+      if ($request == "/embedding") {
+	print_r("In Embedding");
+	$url = 'http://10.1.0.4:8000/embedding';
+        $crl = curl_init($url);
+      // Processing part of the Q&A
+      } elseif ($request == "/qanda") {
+	print_r("In Q&A");
+	$url = 'http://10.1.0.4:8000/qanda';
+        $crl = curl_init($url);
+      }
+      // Frame the POST Request
+      curl_setopt($crl, CURLOPT_POST, 1);
+      curl_setopt($crl, CURLOPT_POSTFIELDS, json_encode($data));
+      curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+      // Execute the POST request
+      $result = curl_exec($crl);
+      echo $result;
+      } else {
+   // Process GET request
+      $url = 'http://10.1.0.4:8000';
+      $response = file_get_contents($url);
+      // Display response
+      echo '<p>Response:</p>';
+      echo '<pre>' . $response . '</pre>';
     }
     ?>
-
-    <form action="" method="POST">
-        <label for="question">Question:</label>
-        <input type="text" name="question" id="question" required><br>
-
-        <button type="submit">Submit</button>
-    </form>
 </body>
 </html>
+
