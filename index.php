6e685025a4205909160e8e2430472c417189af72
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-06-02 16:57:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
     <form action="" method="POST">
         <label for="question">Question:</label>
         <input type="text" name="question" id="question" required><br>
-        <label for="document">Question:</label>
+        <label for="document">Document:</label>
         <input type="text" name="document" id="document" required><br>
 
         <button type="submit">Submit</button>
