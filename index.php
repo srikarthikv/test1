b74256d41390a40c5fc8109d7da310f3d020090e
richard-malchi <131884111+richard-malchi@users.noreply.github.com>
2023-06-04 02:21:49 +0530

    chat bot-like appearance trial

--- index.php
+++ index.php
@@ -8,28 +8,48 @@
             margin: 20px;
         }
 
-        h1 {
-            text-align: center;
+        .chat-container {
+            max-width: 600px;
+            margin: 0 auto;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            padding: 20px;
+            background-color: #f2f2f2;
         }
 
-        form {
-            margin-top: 20px;
+        .message {
+            margin-bottom: 10px;
         }
 
-        label {
-            display: block;
-            margin-bottom: 5px;
+        .user-message {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px;
+            border-radius: 5px;
+            align-self: flex-start;
         }
 
-        input[type="text"] {
-            width: 100%;
+        .bot-message {
+            background-color: #e5e5ea;
+            color: #333;
+            padding: 10px;
+            border-radius: 5px;
+            align-self: flex-end;
+        }
+
+        .input-container {
+            display: flex;
+        }
+
+        .input-container input[type="text"] {
+            flex: 1;
             padding: 5px;
             font-size: 16px;
             border: 1px solid #ccc;
             border-radius: 3px;
         }
 
-        button[type="submit"] {
+        .input-container button[type="submit"] {
             padding: 10px 20px;
             font-size: 16px;
             background-color: #4CAF50;
@@ -37,68 +57,60 @@
             border: none;
             border-radius: 3px;
             cursor: pointer;
-        }
-
-        button[type="submit"]:hover {
-            background-color: #45a049;
-        }
-
-        .response {
-            margin-top: 20px;
-            border: 1px solid #ccc;
-            border-radius: 3px;
-            padding: 10px;
+            margin-left: 10px;
         }
     </style>
 </head>
 <body>
-   <h1>QnA</h1>
+    <div class="chat-container">
+        <div class="message bot-message">Welcome! How can I assist you today?</div>
+        <form action="" method="POST">
+            <div class="input-container">
+                <input type="text" name="question" id="question" required>
+                <button type="submit">Send</button>
+            </div>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Retrieve the question from the form
+            $question = $_POST['question'];
 
-    <form action="" method="POST">
-        <label for="question">Question:</label>
-        <input type="text" name="question" id="question" required><br>
+            // Set the request URL
+            $url = 'https://10.1.0.4:8000/question';
 
-        <button type="submit">Submit</button>
-    </form>
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Retrieve the question from the form
-        $question = $_POST['question'];
+            // Set the request data
+            $data = array(
+                'question' => $question,
+            );
 
-        // Set the request URL
-        $url = 'https://10.1.0.4:8000/question';
+            // Initialize cURL session
+            $ch = curl_init();
 
-        // Set the request data
-        $data = array(
-            'question' => $question,
-        );
+            // Set cURL options
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        // Initialize cURL session
-        $ch = curl_init();
+            // Execute the request
+            $response = curl_exec($ch);
 
-        // Set cURL options
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            // Check for errors
+            if (curl_errno($ch)) {
+                echo '<div class="message bot-message">Error: ' . curl_error($ch) . '</div>';
+            } else {
+                // Output the user's message
+                echo '<div class="message user-message">' . $question . '</div>';
+                // Output the bot's response
+                echo '<div class="message bot-message">' . $response . '</div>';
+            }
 
-        // Execute the request
-        $response = curl_exec($ch);
-
-        // Check for errors
-        if (curl_errno($ch)) {
-            echo 'Error: ' . curl_error($ch);
-        } else {
-            // Output the response
-            echo '<div class="response">' . $response . '</div>';
+            // Close cURL session
+            curl_close($ch);
         }
-
-        // Close cURL session
-        curl_close($ch);
-    }
-    ?>
-
+        ?>
+    </div>
 </body>
 </html>
