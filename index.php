d57f48df6179ac15f72d0505ebccb4ad86c58665
richard-malchi <131884111+richard-malchi@users.noreply.github.com>
2023-06-05 17:52:05 +0530

    error reporting

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -13,46 +12,50 @@
         <input type="file" name="uploadedFile" />
         <input type="submit" value="Upload" />
     </form>
-	<?php
+    <?php
 
-	// Required Azure Blob Storage libraries
-	require_once 'vendor/autoload.php';
+    // Enable error reporting
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-	use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-	use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-	use MicrosoftAzure\Storage\Blob\Models\Blob;
-	use MicrosoftAzure\Storage\Blob\Models\CreateBlockBlobOptions;
+    // Required Azure Blob Storage libraries
+    require_once 'vendor/autoload.php';
 
-	// Azure Blob Storage connection string
-	$connectionString = 'DefaultEndpointsProtocol=https;AccountName=gptdemo7020140432;AccountKey=k3z0/JCQH3yV/9iSceGe+s1dtdIUbp8anSUQ/a0sDsrw34tuFHfd7usPn42bCvjaUdzlfpNvA09O+AStCRDO3w==;EndpointSuffix=core.windows.net';
-	$directoryName = 'documents/';
-	
-	// Create a BlobRestProxy instance
-	$blobClient = BlobRestProxy::createBlobService($connectionString);
+    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+    use MicrosoftAzure\Storage\Blob\Models\Blob;
+    use MicrosoftAzure\Storage\Blob\Models\CreateBlockBlobOptions;
 
-	// Check if a file is uploaded
-	if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploadedFile'])) {
-		$file = $_FILES['uploadedFile'];
+    // Azure Blob Storage connection string
+    $connectionString = 'DefaultEndpointsProtocol=https;AccountName=gptdemo7020140432;AccountKey=k3z0/JCQH3yV/9iSceGe+s1dtdIUbp8anSUQ/a0sDsrw34tuFHfd7usPn42bCvjaUdzlfpNvA09O+AStCRDO3w==;EndpointSuffix=core.windows.net';
+    $directoryName = 'documents/';
 
-		// Generate a unique name for the file
-		$fileName = $directoryName . uniqid() . '_' . $file['name'];
+    // Create a BlobRestProxy instance
+    $blobClient = BlobRestProxy::createBlobService($connectionString);
 
-		// Set the container name where the file will be stored
-		$containerName = 'azureml-blobstore-fa29c537-9f94-4f15-8679-5f1e2fd597e4';
+    // Check if a file is uploaded
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploadedFile'])) {
+        $file = $_FILES['uploadedFile'];
 
-		try {
-		    // Upload the file to Azure Blob Storage
-		    $blobClient->createBlockBlob($containerName, $fileName, fopen($file['tmp_name'], 'r'));
+        // Generate a unique name for the file
+        $fileName = $directoryName . uniqid() . '_' . $file['name'];
 
-		    echo 'File uploaded successfully!';
-		} catch (ServiceException $e) {
-		    $code = $e->getCode();
-		    $error_message = $e->getMessage();
-		    echo "Failed to upload the file. Error message: $error_message";
-		}
-	}
-	?>
-    
+        // Set the container name where the file will be stored
+        $containerName = 'azureml-blobstore-fa29c537-9f94-4f15-8679-5f1e2fd597e4';
+
+        try {
+            // Upload the file to Azure Blob Storage
+            $blobClient->createBlockBlob($containerName, $fileName, fopen($file['tmp_name'], 'r'));
+
+            echo 'File uploaded successfully!';
+        } catch (ServiceException $e) {
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo "Failed to upload the file. Error message: $error_message";
+        }
+    }
+    ?>
+
 </body>
 
 </html>
