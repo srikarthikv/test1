cdac2830a8490f96faa682912e5b212f2ba37adb
richard-malchi <131884111+richard-malchi@users.noreply.github.com>
2023-06-21 11:58:10 +0530

    working copy

--- process_message.php
+++ process_message.php
@@ -9,28 +9,14 @@ if (isset($_POST['send_button'])) {
     $handle = fopen($file, 'a');
 
     // Write the user's message to the file
-    fwrite($handle, "Human: " . $userMessage . PHP_EOL);
+    fwrite($handle, "User: " . $userMessage . PHP_EOL);
 
     // Close the file handle
     fclose($handle);
 
-    // Read the recent 5 interchanges from the file
-    $recentInterchanges = array();
-    $handle = fopen($file, 'r');
-    while (($line = fgets($handle)) !== false) {
-        $recentInterchanges[] = $line;
-        if (count($recentInterchanges) > 5) {
-            array_shift($recentInterchanges);
-        }
-    }
-    fclose($handle);
-
     // Make a request to the Flask server running on your Azure VM
     $flaskServerUrl = 'http://10.1.0.4:8000/get_chatbot_response';
-    $data = array(
-        'user_message' => $userMessage,
-        'context' => implode("", $recentInterchanges)
-    );
+    $data = array('user_message' => $userMessage);
     $options = array(
         'http' => array(
             'method' => 'POST',
@@ -45,7 +31,7 @@ if (isset($_POST['send_button'])) {
     $handle = fopen($file, 'a');
 
     // Write the chatbot's response to the file
-    fwrite($handle, "AI: " . $chatbotResponse . PHP_EOL);
+    fwrite($handle, "Chatbot: " . $chatbotResponse . PHP_EOL);
 
     // Close the file handle
     fclose($handle);
